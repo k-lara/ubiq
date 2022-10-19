66d05f86a07f0dd78c84b78e313b74608e83c93b
Klara <k.brandstatter@ucl.ac.uk>
2022-10-19 20:16:41 +0200

    spawn replayed avatars with saved texture, reduced texture catalogue, prefab switcher also switches textures depending on mode

--- Unity/Assets/Samples/Intro/Scripts/PrefabSwitcher.cs
+++ Unity/Assets/Samples/Intro/Scripts/PrefabSwitcher.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 using Ubiq.Avatars;
 using Ubiq.XR;
 using UnityEngine.UI;
+using Ubiq.Samples;
 
+// depending on switch mode changes either prefab or texture of avatar
 public class PrefabSwitcher : MonoBehaviour
 {
+    public SwitchMode mode;
     public AvatarManager manager;
     public HandController controller;
     public string currentAvatarPrefabUuid;
@@ -16,28 +19,65 @@ public class PrefabSwitcher : MonoBehaviour
 
     private bool prevPressed = false;
 
+    public enum SwitchMode
+    { 
+        Texture = 0,
+        Prefab = 1
+    }
+
+
     // only for VR button
     public void SecondaryButtonPress(bool pressed)
     {
         if (!prevPressed && pressed) // only check for button presses (not releases)
         {
-            currentIdx++;
-            info.text = "Switch Prefab";
-            StartCoroutine(FadeTextToZeroAlpha(1.0f, info));
-
-            if (currentIdx == manager.AvatarCatalogue.prefabs.Count) // loop through prefabs list with each button click
+            if (mode == SwitchMode.Texture)
             {
-                currentIdx = 0;
+                ChangeTexture();
             }
-            currentAvatarPrefab = manager.AvatarCatalogue.prefabs[currentIdx];
-            currentAvatarPrefabUuid = currentAvatarPrefab.name;
-            manager.LocalPrefabUuid = currentAvatarPrefabUuid;
-
-            manager.CreateLocalAvatar(currentAvatarPrefab);
-            Debug.Log("current prefab: " + currentAvatarPrefabUuid);
+            else if (mode == SwitchMode.Prefab)
+            {
+                ChangePrefab();
+            }
         }
         prevPressed = pressed;
     }
+
+    public void ChangePrefab()
+    {
+        currentIdx++;
+
+        if (currentIdx == manager.AvatarCatalogue.prefabs.Count) // loop through prefabs list with each button click
+        {
+            currentIdx = 0;
+        }
+        currentAvatarPrefab = manager.AvatarCatalogue.prefabs[currentIdx];
+        currentAvatarPrefabUuid = currentAvatarPrefab.name;
+        manager.LocalPrefabUuid = currentAvatarPrefabUuid;
+
+        manager.CreateLocalAvatar(currentAvatarPrefab);
+        info.text = "Switch to prefab " + currentAvatarPrefab;
+        StartCoroutine(FadeTextToZeroAlpha(2.0f, info));
+        Debug.Log("current prefab: " + currentAvatarPrefabUuid);
+    }
+
+    public void ChangeTexture()
+    {
+
+        var texturedAvatar = manager.LocalAvatar.gameObject.GetComponent<TexturedAvatar>();
+        string uid = texturedAvatar.GetTextureUuid();
+        int idx = int.Parse(uid);
+        idx++;
+
+        if (idx == texturedAvatar.Textures.Count)
+        {
+            idx = 0;
+        }
+        texturedAvatar.SetTexture(idx.ToString());
+        info.text = "Change to texture " + idx;
+        StartCoroutine(FadeTextToZeroAlpha(2.0f, info));
+    }    
+
     // Start is called before the first frame update
     void Start()
     {
